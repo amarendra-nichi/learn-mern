6b2065c4f9d7e2f0c0185d8165dd60425a5a6426
HarishNichi <harish.devarayal@nichi.com>
2022-02-18 18:33:20 +0530

    CSV Export

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var XYZController = require("../controllers/XYZController");
+var studentController=require("../controllers/studentController")
 /* GET home page. */
 router.get("/", XYZController.index);
 router.get("/create", XYZController.create);
@@ -8,5 +9,5 @@ router.post("/insert", XYZController.insert);
 router.post("/update", XYZController.update);
 router.post("/delete", XYZController.delete);
 router.get("/getAll", XYZController.getAll);
-
+router.get("/getStudents", studentController.getAll);
 module.exports = router;
